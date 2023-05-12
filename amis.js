ef81ffb3fd90c05586dc6b0de9464d7c6d12f10c
foufouboy <37068293+foufouboy@users.noreply.github.com>
2023-05-12 13:53:30 +0200

    Changé la borne supérieure à 1000

--- amis.js
+++ amis.js
@@ -39,7 +39,7 @@ iterationSlt.addEventListener("input", _ => {
 
 function validInput(input) {
     input = +input;
-    return (0 <= input && input <= 200) && Number.isInteger(input)
+    return (0 <= input && input <= 1000) && Number.isInteger(input)
 }
 
 function shuffle(a) {
